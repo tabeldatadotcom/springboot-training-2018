11e4f6fcf8b7acb6fe29504e7910e55a5d9f131a
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-10-02 15:56:13 +0700

    update project

--- src/main/java/com/tabeldata/training/springbootdemo/controller/HaloController.java
+++ src/main/java/com/tabeldata/training/springbootdemo/controller/HaloController.java
@@ -5,6 +5,7 @@
  */
 package com.tabeldata.training.springbootdemo.controller;
 
+import com.tabeldata.training.springbootdemo.dto.RequestHalo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,8 +23,11 @@ public class HaloController {
     
     @GetMapping(
             path = "/findByName")
-    public String halo(@RequestParam String nama){
-        String format = String.format("{ \"message\": \"halo ini dari spring\", \"nama\" : \"%s\" }", nama);
+    public String halo(
+            @RequestParam(name = "nama", required = false) 
+                    String param){
+        String format = String.format("{ \"message\": \"halo ini dari spring\", \"nama\" : \"%s\" }",
+                param);
         return format;
     }
     
@@ -31,8 +35,9 @@ public class HaloController {
             path = "/findByName")
     public String haloPost(
             @RequestParam String nama, 
-            @RequestBody String alamat){
-        String format = String.format("{ \"message\": \"halo ini dari spring\", \"nama\" : \"%s\" }", nama);
+            @RequestBody RequestHalo request){
+        String format = String.format("{ \"message\": \"halo ini dari spring\", \"nama\" : \"%s\" }", 
+                request.getNama());
         return format;
     }
     
---
+++ src/main/java/com/tabeldata/training/springbootdemo/dto/RequestHalo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tabeldata.training.springbootdemo.dto;
+
+import lombok.Data;
+
+/**
+ *
+ * @author dimasm93
+ */
+@Data
+public class RequestHalo {
+    
+    private String nama;
+    private String alamat;
+    
+}
