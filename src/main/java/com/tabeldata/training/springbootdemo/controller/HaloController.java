50e8c33521c0946d973e5e1efd2cdc8b435402fe
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-10-02 15:32:04 +0700

    update request get & request post

--- src/main/java/com/tabeldata/training/springbootdemo/controller/HaloController.java
+++ src/main/java/com/tabeldata/training/springbootdemo/controller/HaloController.java
@@ -5,8 +5,11 @@
  */
 package com.tabeldata.training.springbootdemo.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,14 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @author dimasm93
  */
 @RestController
+@RequestMapping("/halo")
 public class HaloController {
     
     @GetMapping(
-            path = "/", 
-            consumes = MediaType.APPLICATION_JSON_VALUE, 
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String halo(){
-        return "{ \"message\" : \"Halo ini dari spring web mvc\"}";
+            path = "/findByName")
+    public String halo(@RequestParam String nama){
+        String format = String.format("{ \"message\": \"halo ini dari spring\", \"nama\" : \"%s\" }", nama);
+        return format;
+    }
+    
+    @PostMapping(
+            path = "/findByName")
+    public String haloPost(
+            @RequestParam String nama, 
+            @RequestBody String alamat){
+        String format = String.format("{ \"message\": \"halo ini dari spring\", \"nama\" : \"%s\" }", nama);
+        return format;
     }
     
 }
