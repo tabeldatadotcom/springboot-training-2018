fccec44ad6c9ca9fce31a5a9fd6ddda8f40c991e
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-10-02 10:01:38 +0700

    update spring connect mysql dbase pom

--- src/main/java/com/tabeldata/training/springbootdemo/bean/DataBean.java
+++ src/main/java/com/tabeldata/training/springbootdemo/bean/DataBean.java
@@ -54,8 +54,6 @@ public class DataBean {
         this.bean = bean;
     }
     
-    
-
     @Override
     public String toString() {
         return String.format("{%s, %s, otherBean: { %s, %s }, %s}", 
