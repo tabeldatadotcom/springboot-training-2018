49385d7c9dd7fee49aaeab9d1130c7838178e7d4
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-10-03 14:19:51 +0700

    fitur create update delete dengan jdbc template

--- src/main/java/com/tabeldata/training/springbootdemo/controller/ProdukController.java
+++ src/main/java/com/tabeldata/training/springbootdemo/controller/ProdukController.java
@@ -11,8 +11,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +45,24 @@ public class ProdukController {
     @GetMapping("/list")
     public List<Produk> findAll(){
         return dao.findAll();
+    }
+    
+    @PostMapping("/")
+    public ResponseEntity<Produk> save(@RequestBody Produk produk){
+        produk = this.dao.save(produk);
+        return ResponseEntity.ok(produk);
+    }
+    
+    @PutMapping("/")
+    public ResponseEntity<Produk> update(@RequestBody Produk produk){
+        produk = this.dao.update(produk);
+        return ResponseEntity.ok(produk);
+    }
+    
+    @DeleteMapping("/{kode}")
+    public ResponseEntity<Produk> delete(@PathVariable("kode") String kode){
+        this.dao.deleteById(kode);
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/com/tabeldata/training/springbootdemo/dao/ProdukDao.java
+++ src/main/java/com/tabeldata/training/springbootdemo/dao/ProdukDao.java
@@ -6,14 +6,15 @@
 package com.tabeldata.training.springbootdemo.dao;
 
 import com.tabeldata.training.springbootdemo.entity.Produk;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-import javax.sql.DataSource;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
@@ -23,41 +24,83 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class ProdukDao {
-    
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
-    public Produk findById(String id) throws EmptyResultDataAccessException{
+
+    public Produk findById(String id) throws EmptyResultDataAccessException {
         String query = "select id as kode_unique, kode, nama, harga, qty from produk where id = ?";
-        Produk produk = jdbcTemplate.queryForObject(query, new RowMapper<Produk>(){
+        Produk produk = jdbcTemplate.queryForObject(query, new RowMapper<Produk>() {
             @Override
             public Produk mapRow(ResultSet rs, int i) throws SQLException {
                 return new Produk(
                         rs.getString("kode_unique"),
-                        rs.getString("kode"), 
+                        rs.getString("kode"),
                         rs.getString("nama"),
-                        rs.getBigDecimal("harga"), 
+                        rs.getBigDecimal("harga"),
                         rs.getInt("qty"));
             }
-            
+
         }, id);
         return produk;
     }
-   
-    public List<Produk> findAll(){
+
+    public List<Produk> findAll() {
         String query = "select id as kode_unique, kode, nama, harga, qty from produk";
-        List<Produk> list = jdbcTemplate.query(query, new RowMapper<Produk>(){
+        List<Produk> list = jdbcTemplate.query(query, new RowMapper<Produk>() {
             @Override
             public Produk mapRow(ResultSet rs, int i) throws SQLException {
                 return new Produk(
                         rs.getString("kode_unique"),
-                        rs.getString("kode"), 
+                        rs.getString("kode"),
                         rs.getString("nama"),
-                        rs.getBigDecimal("harga"), 
+                        rs.getBigDecimal("harga"),
                         rs.getInt("qty"));
             }
         });
         return list;
     }
+
+    public Produk save(Produk produk) {
+        String query = "insert into produk(id, kode, nama, harga, qty) values (? ,?, ?, ?, ?)";
+        String primaryKey = UUID.randomUUID().toString();
+        this.jdbcTemplate.update(query, new PreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps) throws SQLException {
+                ps.setString(1, primaryKey);
+                ps.setString(2, produk.getKode());
+                ps.setString(3, produk.getNama());
+                ps.setBigDecimal(4, produk.getHarga());
+                ps.setInt(5, produk.getQty());
+            }
+        });
+        produk.setId(primaryKey);
+        return produk;
+    }
     
+    public Produk update(Produk produk) {
+        String query = "update produk set kode = ?, nama = ?, harga = ?, qty = ? where id = ?";
+        this.jdbcTemplate.update(query, new PreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps) throws SQLException {
+                ps.setString(1, produk.getKode());
+                ps.setString(2, produk.getNama());
+                ps.setBigDecimal(3, produk.getHarga());
+                ps.setInt(4, produk.getQty());
+                ps.setString(5, produk.getId());
+            }
+        });
+        return produk;
+    }
+    
+    public void deleteById(String id){
+       String query = "delete from produk where id = ?";
+        this.jdbcTemplate.update(query, new PreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps) throws SQLException {                
+                ps.setString(1, id);
+            }
+        });
+    }
+
 }
