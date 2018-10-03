3aea46f0b8856aab31179ba4b4a44ebd20559ea8
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-10-03 10:27:47 +0700

    update project

--- README.md
+++ README.md
@@ -12,3 +12,25 @@ grant all privileges on *.* to bootcamp@'%' identified by 'bootcamp';
 # database bootcamp;
 create database bootcamp;
 ```
+
+## How to import sql file to mysql
+
+```bash
+## change directory to file sql
+cd src/main/resources/db/migration 
+
+## connect to mysql bootcamp database
+mysql -h 127.0.0.1 -u bootcamp -p
+
+## use database bootcamp
+use bootcamp;
+
+## import sql file to database bootcamp
+source schema_database.sql
+
+## check tables
+show tables;
+
+## select rows data
+select * from produk;
+```
---
+++ src/main/resources/db/migration/schema_database.sql
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  dimasm93
+ * Created: Oct 3, 2018
+ */
+
+create table produk(
+    id varchar(255) not null primary key,
+    kode varchar(50) not null unique,
+    nama varchar(100) not null,
+    harga decimal not null default 0,
+    qty int not null default 0
+) engine = InnoDB;
+
+insert into produk(id, kode, nama, harga, qty) values 
+    ('produk-001', 'produk-001', 'Macbook Pro 13 inch', 28000000, 5),
+    ('produk-002', 'produk-002', 'Macbook Pro 15 inch', 50000000, 2),
+    ('produk-003', 'produk-003', 'Apple watch', 3000000, 1); 
+    
