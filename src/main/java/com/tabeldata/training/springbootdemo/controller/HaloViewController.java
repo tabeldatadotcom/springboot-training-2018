8883c6cb52ddcb3e539b87c7e5e1f273765a71b9
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-10-17 13:57:03 +0700

    memangkap request parameter dari request url

--- src/main/java/com/tabeldata/training/springbootdemo/controller/HaloViewController.java
+++ src/main/java/com/tabeldata/training/springbootdemo/controller/HaloViewController.java
@@ -8,6 +8,8 @@ package com.tabeldata.training.springbootdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -17,8 +19,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HaloViewController {
     
     @GetMapping("/halo")
-    public String halo(){
-        return "home";
+    public ModelAndView halo(
+            ModelAndView view,
+            @RequestParam(name = "nama", required = false) String nama){
+        view.setViewName("home");
+        view.addObject("namaLengkap", nama);
+        return view;
     }
     
 }
